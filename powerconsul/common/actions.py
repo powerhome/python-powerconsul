e8b325b69d0b4fdfe82d3bd69bc80d96fb38e4ba
David Taylor <djtaylor13@gmail.com>
2016-11-08 11:48:39 -0500

    Basic functionality for enabling/disabling a crontab

--- powerconsul/common/actions.py
+++ powerconsul/common/actions.py
@@ -1,16 +1,36 @@
+from os import rename, path
+
 class PowerConsul_Actions(object):
     """
     Command actions to be run by triggered events.
     """
     @classmethod
     def enableCrontab(cls, **kwargs):
-        cronUser = kwargs.get('user')
-        POWERCONSUL.LOG.info('Enabling crontab for user: {0}'.format(cronUser))
+        cronUser    = kwargs.get('user')
+        cronPath    = '/var/spool/cron/crontabs.disabled/{0}'.format(cronUser)
+        cronEnabled = '/var/spool/cron/crontabs/{0}'.format(cronUser)
+
+        # Check if the crontab is in the disabled directory
+        if not path.isfile(cronPath):
+            raise Exception("Cannot enable crontab for user [{0}]: file not found: {1}".format(cronUser, cronPath))
+
+        # Enable the crontab
+        rename(cronPath, cronEnabled)
+        POWERCONSUL.LOG.info('Enabled crontab for user: {0}'.format(cronUser))
 
     @classmethod
     def disableCrontab(cls, **kwargs):
-        cronUser = kwargs.get('user')
-        POWERCONSUL.LOG.info('Disabling crontab for user: {0}'.format(cronUser))
+        cronUser     = kwargs.get('user')
+        cronPath     = '/var/spool/cron/crontabs/{0}'.format(cronUser)
+        cronDisabled = '/var/spool/cron/crontabs.disabled/{0}'.format(cronUser)
+
+        # Check if the crontab is in the disabled directory
+        if not path.isfile(cronPath):
+            raise Exception("Cannot disable crontab for user [{0}]: file not found: {1}".format(cronUser, cronPath))
+
+        # Enable the crontab
+        rename(cronPath, cronDisabled)
+        POWERCONSUL.LOG.info('Disabled crontab for user: {0}'.format(cronUser))
 
     @classmethod
     def get(cls, method):
