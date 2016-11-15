26c1e157af33d0c43ffb2e7202ffeda6e52128db
David Taylor <djtaylor13@gmail.com>
2016-11-15 09:44:54 -0500

    Create the process check handler (Nagios plugin wrapper)

---
+++ powerconsul/common/checks/process.py
@@ -0,0 +1,106 @@
+from os import path
+from subprocess import Popen, PIPE
+
+from powerconsul.common.checks import Check_Base
+
+class Check_Process(Check_Base):
+    """
+    Class object representing a process check.
+    """
+    def __init__(self):
+        super(Check_Process, self).__init__('process')
+
+        # Nagios check_procs script
+        self.nagiosScript = '/usr/lib/nagios/plugins/check_procs'
+
+        # Nagios script arguments
+        self.nagiosArgs   = POWERCONSUL.ARGS.get('nagiosargs',
+            required='Nagios arguments required: powerconsul check process -n \'<nagiosArgs>\''
+        )
+
+    def checkNagios(self):
+        """
+        Check if a process is healthy via Nagios checks.
+        """
+
+        # The Nagios script must exist
+        if not path.isfile(self.nagiosScript):
+            POWERCONSUL.die('Unable to locate Nagios process check script: {0}'.format(self.nagiosScript))
+
+        # Execute the health check
+        proc     = Popen([self.nagiosScript] + self.nagiosArgs.split(' '), stdout=PIPE, stderr=PIPE)
+        out, err = proc.communicate()
+
+        # Failed to run Nagios check (invalid syntax)
+        if proc.returncode == 3:
+            POWERCONSUL.die('Failed to run Nagios check [{0}]: {1}'.format(self.nagiosScript, err.rstrip()))
+
+        # Return the exit code and output
+        return proc.returncode, out.rstrip()
+
+    def ensure(self, expects=True, clustered=False, active=True):
+        """
+        Ensure a specific service state.
+        """
+        code, output = self.checkNagios()
+
+        # Process should be running/healthy
+        if expects == True:
+
+            # Passing
+            if code == 0:
+                POWERCONSUL.OUTPUT.passing({
+                    'type': 'process',
+                    'nagiosScript': self.nagiosScript,
+                    'nagiosArgs': self.nagiosArgs,
+                    'nagiosOutput': output,
+                    'expects': expects,
+                    'clustered': clustered
+                })
+
+            # Warning
+            if code == 1:
+                POWERCONSUL.OUTPUT.warning({
+                    'type': 'process',
+                    'nagiosScript': self.nagiosScript,
+                    'nagiosArgs': self.nagiosArgs,
+                    'nagiosOutput': output,
+                    'expects': expects,
+                    'clustered': clustered
+                })
+
+            # Critical
+            if code == 2:
+                POWERCONSUL.OUTPUT.critical({
+                    'type': 'process',
+                    'nagiosScript': self.nagiosScript,
+                    'nagiosArgs': self.nagiosArgs,
+                    'nagiosOutput': output,
+                    'expects': expects,
+                    'clustered': clustered
+                })
+
+        # Process should be stopped
+        if expects == False:
+
+            # Passing (Nagios check == critical)
+            if code == 2:
+                POWERCONSUL.OUTPUT.passing({
+                    'type': 'process',
+                    'nagiosScript': self.nagiosScript,
+                    'nagiosArgs': self.nagiosArgs,
+                    'nagiosOutput': output,
+                    'expects': expects,
+                    'clustered': clustered
+                })
+
+            # Critical (Nagios check == warning/passing)
+            if code in [0, 1]:
+                POWERCONSUL.OUTPUT.critical({
+                    'type': 'process',
+                    'nagiosScript': self.nagiosScript,
+                    'nagiosArgs': self.nagiosArgs,
+                    'nagiosOutput': output,
+                    'expects': expects,
+                    'clustered': clustered
+                })
